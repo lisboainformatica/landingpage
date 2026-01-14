26b71c485f6d6f838149fab021a26e5c35d49688
lisboainformatica <gestaolisboainfo@gmail.com>
2026-01-14 15:00:49 -0300

    Simplify AOS initialization in AosInit component
    Refactor AOS initialization to use a single useEffect hook with configuration options.

--- src/app/_components/aos-init.tsx
+++ src/app/_components/aos-init.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
-import Aos from "aos";
+import AOS from "aos";
 import "aos/dist/aos.css";
 
 export function AosInit() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      duration: 800,
+      easing: "ease-out-cubic",
+    });
+  }, []);
 
-    useEffect( () => {}, []);
-      Aos.init({ 
-        duration: 800, 
-        once: true
-      });
   return null;
 }
